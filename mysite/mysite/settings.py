b2a5110685ee55a2f3fdab466afb6461bd58660e
carmonaalex12 <carmona2@uci.edu>
2017-07-05 13:47:03 -0700

    added Procfile and Heroku set up stuff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h&j+0egav*&4ptj9e=&hmt%y%29)(ys+_v#*#40!tzcm8=#+k7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'laverniasite.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'laverniasite.pythonanywhere.com','laverniasite.herokuapp.com/']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+'''
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+'''
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
